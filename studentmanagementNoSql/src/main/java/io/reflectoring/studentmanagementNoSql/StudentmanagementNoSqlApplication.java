cff911035be23c809abcbc6538013893a46ec15f
Git Demo <neerajmalviya7746@gmail.com>
2025-04-16 18:01:01 +0530

    it is first comment for project using studentmanagemenNoSql

---
+++ studentmanagementNoSql/src/main/java/io/reflectoring/studentmanagementNoSql/StudentmanagementNoSqlApplication.java
@@ -0,0 +1,13 @@
+package io.reflectoring.studentmanagementNoSql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentmanagementNoSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentmanagementNoSqlApplication.class, args);
+	}
+
+}
---
+++ studentmanagementNoSql/src/main/java/io/reflectoring/studentmanagementNoSql/entity/Student.java
@@ -0,0 +1,52 @@
+package io.reflectoring.studentmanagementNoSql.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+@Document(collection="Students")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    @Id
+    String id;
+    String name;
+    String department;
+    int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
